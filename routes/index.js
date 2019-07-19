2a7f249f9993b1e9c696b42674ccc6a98206b7d9
Mikis Woodwinter <mikis.woodwinter@gmail.com>
2019-07-19 10:07:42 -0700

    Add POST /projects route
    Signed-off-by: Mikis Woodwinter <mikis.woodwinter@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const { Router } = require("express")
+const { BadRequest } = require("http-errors")
 
-const { getProjectById } = require("../model")
+const { getProjectById, addProject } = require("../model")
 
 const apiRouter = Router()
 
@@ -9,6 +10,16 @@ apiRouter.get("/projects/:projectId", async (req, res, next) => {
         const project = await getProjectById(req.params.projectId)
         res.json(project)
     } catch (error) {
+        next(error)
+    }
+})
+
+apiRouter.post("/projects", async (req, res, next) => {
+    try {
+        const addedProject = await addProject(req.body)
+        res.json(addedProject)
+    } catch (error) {
+        if (error.errno === 19) return next(BadRequest(error.message))
         next(error)
     }
 })
