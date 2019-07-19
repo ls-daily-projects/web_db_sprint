08e3592ef52135f7be102e4aabb3ca1102f44487
Mikis Woodwinter <mikis.woodwinter@gmail.com>
2019-07-19 09:08:12 -0700

    Setup fake data w/ faker
    Signed-off-by: Mikis Woodwinter <mikis.woodwinter@gmail.com>

---
+++ data/utils/index.js
@@ -0,0 +1,34 @@
+const faker = require("faker")
+
+const maxRecords = 100
+
+const createMany = (factory, count = maxRecords) => {
+    const many = []
+    for (let index = 0; index < count; index++) {
+        many.push(factory())
+    }
+    return many
+}
+
+const createProject = () => {
+    return {
+        name: faker.lorem.words(7),
+        description: faker.lorem.text(1),
+        isCompleted: faker.random.arrayElement([true, false])
+    }
+}
+
+const createAction = () => {
+    return {
+        description: faker.lorem.words(7),
+        notes: faker.lorem.text(1),
+        isCompleted: faker.random.arrayElement([true, false]),
+        project_id: faker.random.number({ min: 1, max: maxRecords })
+    }
+}
+
+module.exports = {
+    createMany,
+    createProject,
+    createAction
+}
